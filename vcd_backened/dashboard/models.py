c74c8539b2c4175d7746e568964606342329a1f8
satyendra123 <Singhsatyendra885@gmail.com>
2025-01-30 13:10:20 +0530

    Update models.py

--- vcd_backened/dashboard/models.py
+++ vcd_backened/dashboard/models.py
@@ -7,3 +7,18 @@ class CarLog(models.Model):
     action = models.CharField(max_length=64, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+from django.db import models
+from django.utils import timezone
+
+class Boomsig(models.Model):
+    id = models.AutoField(primary_key=True)
+    entryboom = models.CharField(max_length=1, default='N')
+    entrysynctime = models.DateTimeField(default=timezone.now)
+    exitboom = models.CharField(max_length=1, default='N')
+    exitsynctime = models.DateTimeField(default=timezone.now)
+    barrier_open_count = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = 'boomsig'
